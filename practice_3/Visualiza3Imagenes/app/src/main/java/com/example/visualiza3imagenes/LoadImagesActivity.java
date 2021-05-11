97066296386794734317a189ce58bed5f236ac7d
luisblazquezm <luisblazquezm@usal.es>
2021-05-11 19:43:26 +0200

    [feat] Still crashing :angry:

--- practice_3/Visualiza3Imagenes/app/src/main/java/com/example/visualiza3imagenes/LoadImagesActivity.java
+++ practice_3/Visualiza3Imagenes/app/src/main/java/com/example/visualiza3imagenes/LoadImagesActivity.java
@@ -1,5 +1,6 @@
 package com.example.visualiza3imagenes;
 
+import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
@@ -16,6 +17,7 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class LoadImagesActivity extends AppCompatActivity {
 
@@ -23,13 +25,10 @@ public class LoadImagesActivity extends AppCompatActivity {
     private ImageView imageView;
     private ProgressBar loadImageProgressBarr;
     private TextView loadImageTextLabel;
-    private Handler progressBarbHandler = new Handler();
     private TextView endMessageTextView;
 
     // CONSTANTS
     private final int[] imageIds = {0, 1, 2};
-    private int progressBarStatus = 0;
-    private int imageIdToShow = -1;
     private final int DOG_IMG_ID = 0;
     private final int CAT_IMG_ID = 1;
     private final int LION_IMG_ID = 2;
@@ -45,131 +44,127 @@ public class LoadImagesActivity extends AppCompatActivity {
 
         // Load UI element to load images
         this.imageView = (ImageView) findViewById(R.id.imageViewCatDogAndLion);
-        this.loadImageProgressBarr = (ProgressBar) findViewById(R.id.loadImageProgressBar);
+        this.loadImageProgressBarr = (ProgressBar) findViewById(R.id.loadImageProgressBarr);
         this.loadImageTextLabel = (TextView) findViewById(R.id.loadImageMessageTextView);
         this.endMessageTextView = (TextView) findViewById(R.id.endMessageTextView);
+
+        // Initialize progress barr
+        loadImageProgressBarr.setMax(100);
+        loadImageProgressBarr.setProgress(0);
 
         // Hide image view and message
         this.imageView.setVisibility(View.GONE);
         this.endMessageTextView.setVisibility(View.GONE);
 
-        // Ask secondary threads to load images
-        this.loadBarr();
-    }
-
-    public void setImg(int imageIdToShow) {
-
-        // Hide load label and spinner
-        loadImageProgressBarr.setVisibility(View.GONE);
-        loadImageTextLabel.setVisibility(View.GONE);
-
-        // Display imageView hidden
-        imageView.setVisibility(View.VISIBLE);
-
-        Log.w("setImg", "Load image: " + imageIdToShow);
-
-        // Set image depending on the image
-        if (DOG_IMG_ID == imageIdToShow)
-            imageView.setImageResource(R.drawable.perro);
-        else if (CAT_IMG_ID == imageIdToShow)
-            imageView.setImageResource(R.drawable.gato);
-        else if (LION_IMG_ID == imageIdToShow)
-            imageView.setImageResource(R.drawable.leon);
-        else
-            imageView.setImageResource(R.drawable.perro);
-
-        Log.w("setImg", "Wait 3 SECONDS");
-
-        // Visualize during 3 seconds
-        try {
-            Thread.sleep(IMAGE_VISUALIZATION_TIME); // 3 seconds
-        } catch (Exception ex) {
-        }
-
-        Log.w("setImg", "Finish visualization");
-
-        Log.w("doInBackground", "imageIdToShow is " + imageIdToShow);
-
-        // Hide image view
-        imageView.setVisibility(View.GONE);
-
-        if (imageIdToShow >= imageIds.length) {
-            // Main thread ends tasks
-            loadImageProgressBarr.setVisibility(View.GONE);
-            loadImageTextLabel.setVisibility(View.GONE);
-            endMessageTextView.setVisibility(View.VISIBLE);
-        } else {
-            // Display load image label and progress barr
-            loadImageProgressBarr.setVisibility(View.VISIBLE);
-            loadImageTextLabel.setVisibility(View.VISIBLE);
-        }
-    }
-
-    public void loadBarr() {
-
         // Main thread before loading images
         loadImageProgressBarr.setVisibility(View.VISIBLE);
         loadImageTextLabel.setVisibility(View.VISIBLE);
 
-        //for (int imageId : imageIds){
+        // Ask secondary threads to load images
+        this.getImgFromThread();
+    }
 
-            Thread th = new HandlerThread("UIHandler"){
+    private void getImgFromThread() {
 
-                @Override
-                public void run() {
-                    // Generate aleatory number
-                    int loadAleatoryTime = new Random().nextInt((MAX_ALEATORY_NUMBER - MIN_ALEATORY_NUMBER) + 1) + MIN_ALEATORY_NUMBER;
+        // Initialization and Start threads
+        for (int i = 0; i < this.imageIds.length; i++) {
+            LoadImagesThread th = new LoadImagesThread(this.imageIds[i]);
+            th.start();
+        }
+    }
 
-                    // Set max and min for barr
-                    runOnUiThread(new Runnable() {
-                      public void run() {
-                          loadImageProgressBarr.setMax(loadAleatoryTime);
-                          loadImageProgressBarr.setProgress(0);
-                      }
-                    });
+    class LoadImagesThread extends Thread{
+        private int i;
+        private int imageId;
 
-                    // Make thread simulate is loading the image
-                    for (progressBarStatus = 0; progressBarStatus <= loadAleatoryTime; progressBarStatus++) {
-
-                        progressBarbHandler.post(new Runnable() {
-                            public void run() {
-                                runOnUiThread(new Runnable() {
-                                    public void run() {
-                                        loadImageProgressBarr.setProgress(progressBarStatus);
-                                    }
-                                });
-                            }
-                        });
-
-                        // Wait for every second
-                        try {
-                            Thread.sleep(MILLISECOND_TIME); // 1 second
-                        } catch (Exception ex) {
-                        }
-                        Log.w("doInBackground", "Progress is " + progressBarStatus + " / " + loadAleatoryTime);
-                    }
-
-                    //imageIdToShow = imageId;
-
-                    // Reset status
-                    progressBarStatus = 0;
-                }
-            };
-
-        //}
-
-        // Start thread
-        th.start();
-
-        // Wait for thread to finish
-        try {
-            th.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        public LoadImagesThread(int imageId) {
+            this.imageId = imageId;
         }
 
-        // Load image
-        setImg(0);
+        public void setImg() {
+
+            // Hide load label and spinner
+            loadImageProgressBarr.setVisibility(View.GONE);
+            loadImageTextLabel.setVisibility(View.GONE);
+
+            // Display imageView hidden
+            imageView.setVisibility(View.VISIBLE);
+
+            Log.w("setImg", "Load image: " + this.imageId);
+
+            // Set image depending on the image
+            if (DOG_IMG_ID == this.imageId)
+                imageView.setImageResource(R.drawable.perro);
+            else if (CAT_IMG_ID == this.imageId)
+                imageView.setImageResource(R.drawable.gato);
+            else if (LION_IMG_ID == this.imageId)
+                imageView.setImageResource(R.drawable.leon);
+            else
+                imageView.setImageResource(R.drawable.perro);
+
+            Log.w("setImg", "Wait 3 SECONDS");
+
+            // Wait for every second
+            try {
+                Thread.sleep(IMAGE_VISUALIZATION_TIME); // 1 second
+            } catch (Exception ex) {
+            }
+
+            Log.w("setImg", "Finish visualization");
+
+            Log.w("doInBackground", "imageIdToShow is " + this.imageId);
+
+            // Hide image view
+            imageView.setVisibility(View.GONE);
+
+            if (this.imageId >= imageIds.length) {
+                // Main thread ends tasks
+                loadImageProgressBarr.setVisibility(View.GONE);
+                loadImageTextLabel.setVisibility(View.GONE);
+                endMessageTextView.setVisibility(View.VISIBLE);
+            } else {
+                // Display load image label and progress barr
+                loadImageProgressBarr.setVisibility(View.VISIBLE);
+                loadImageTextLabel.setVisibility(View.VISIBLE);
+            }
+        }
+
+        private void publishProgress(int progressValue, int randomTime) {
+            int progress = progressValue * 100 / randomTime;
+
+            loadImageProgressBarr.setProgress(progress * 10);
+        }
+
+        @Override
+        public void run() {
+
+            // Generate aleatory number
+            int loadAleatoryTime = new Random().nextInt((MAX_ALEATORY_NUMBER - MIN_ALEATORY_NUMBER) + 1) + MIN_ALEATORY_NUMBER;
+
+            // Make thread simulate is loading the image
+            for (i = 0; i <= loadAleatoryTime; i++) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishProgress(i, loadAleatoryTime);
+                    }
+                });
+                // Wait for every second
+                try {
+                    Thread.sleep(MILLISECOND_TIME); // 1 second
+                } catch (Exception ex) {
+                }
+            }
+
+            // Set barr when thread finish
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    publishProgress(loadAleatoryTime, loadAleatoryTime);
+                    setImg();
+                }
+            });
+        }
     }
 
 }
