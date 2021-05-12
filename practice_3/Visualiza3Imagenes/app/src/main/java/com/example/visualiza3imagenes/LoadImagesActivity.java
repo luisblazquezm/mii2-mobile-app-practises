76923d77d593ce44259d0ec4b66f41eae53da5b6
luisblazquezm <luisblazquezm@usal.es>
2021-05-12 22:23:00 +0200

    [P3][feat] Added new files

--- practice_3/Visualiza3Imagenes/app/src/main/java/com/example/visualiza3imagenes/LoadImagesActivity.java
+++ practice_3/Visualiza3Imagenes/app/src/main/java/com/example/visualiza3imagenes/LoadImagesActivity.java
@@ -4,12 +4,15 @@ import androidx.annotation.MainThread;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.os.Parcelable;
 import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -26,6 +29,7 @@ public class LoadImagesActivity extends AppCompatActivity {
     private ProgressBar loadImageProgressBarr;
     private TextView loadImageTextLabel;
     private TextView endMessageTextView;
+    private Button endActivityButton;
 
     // CONSTANTS
     private final int[] imageIds = {0, 1, 2};
@@ -37,6 +41,10 @@ public class LoadImagesActivity extends AppCompatActivity {
     private final long MILLISECOND_TIME = 1000; // 1 second
     private final long IMAGE_VISUALIZATION_TIME = 3 * MILLISECOND_TIME; // 3 seconds in milliseconds
 
+    // OTHER
+    private boolean hasActivityFinished = false;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,35 +54,54 @@ public class LoadImagesActivity extends AppCompatActivity {
         this.imageView = (ImageView) findViewById(R.id.imageViewCatDogAndLion);
         this.loadImageProgressBarr = (ProgressBar) findViewById(R.id.loadImageProgressBarr);
         this.loadImageTextLabel = (TextView) findViewById(R.id.loadImageMessageTextView);
+        this.endActivityButton = (Button) findViewById(R.id.endActivityButton);
         this.endMessageTextView = (TextView) findViewById(R.id.endMessageTextView);
 
         // Initialize progress barr
         loadImageProgressBarr.setMax(100);
         loadImageProgressBarr.setProgress(0);
 
-        // Hide image view and message
-        this.imageView.setVisibility(View.GONE);
-        this.endMessageTextView.setVisibility(View.GONE);
+        if (savedInstanceState == null && !hasActivityFinished) {
+            // Hide image view and message
+            this.imageView.setVisibility(View.GONE);
+            this.endMessageTextView.setVisibility(View.GONE);
+            this.endActivityButton.setVisibility(View.GONE);
 
-        // Main thread before loading images
-        loadImageProgressBarr.setVisibility(View.VISIBLE);
-        loadImageTextLabel.setVisibility(View.VISIBLE);
+            // Main thread before loading images
+            loadImageProgressBarr.setVisibility(View.VISIBLE);
+            loadImageTextLabel.setVisibility(View.VISIBLE);
 
-        // Ask secondary threads to load images
-        this.getImgFromThread();
+            // Ask secondary threads to load images
+            this.getImgFromThread();
+        } else {
+            this.loadImageProgressBarr.setVisibility(View.GONE);
+            this.loadImageTextLabel.setVisibility(View.GONE);
+            this.endMessageTextView.setVisibility(View.VISIBLE);
+            this.endActivityButton.setVisibility(View.VISIBLE);
+        }
     }
 
     private void getImgFromThread() {
 
         // Initialization and Start threads
-        for (int i = 0; i < this.imageIds.length; i++) {
-            LoadImagesThread th = new LoadImagesThread(this.imageIds[i]);
-            th.start();
-        }
+        new Thread(()->{
+            for (int imageId : this.imageIds){
+                LoadImagesThread th = new LoadImagesThread(imageId);
+                th.start();
+                try {
+                    th.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    public void exitActivity(View view) {
+        finish();
     }
 
     class LoadImagesThread extends Thread{
-        private int i;
         private int imageId;
 
         public LoadImagesThread(int imageId) {
@@ -83,56 +110,70 @@ public class LoadImagesActivity extends AppCompatActivity {
 
         public void setImg() {
 
-            // Hide load label and spinner
-            loadImageProgressBarr.setVisibility(View.GONE);
-            loadImageTextLabel.setVisibility(View.GONE);
+            runOnUiThread(()->{
+                // Hide load label and spinner
+                loadImageProgressBarr.setVisibility(View.GONE);
+                loadImageTextLabel.setVisibility(View.GONE);
 
-            // Display imageView hidden
-            imageView.setVisibility(View.VISIBLE);
+                // Display imageView hidden
+                imageView.setVisibility(View.VISIBLE);
 
-            Log.w("setImg", "Load image: " + this.imageId);
+                Log.w("setImg", "Load image: " + this.imageId);
 
-            // Set image depending on the image
-            if (DOG_IMG_ID == this.imageId)
-                imageView.setImageResource(R.drawable.perro);
-            else if (CAT_IMG_ID == this.imageId)
-                imageView.setImageResource(R.drawable.gato);
-            else if (LION_IMG_ID == this.imageId)
-                imageView.setImageResource(R.drawable.leon);
-            else
-                imageView.setImageResource(R.drawable.perro);
+                // Set image depending on the image
+                switch (this.imageId){
+                    case CAT_IMG_ID:
+                        imageView.setImageResource(R.drawable.gato);
+                        break;
+                    case LION_IMG_ID:
+                        imageView.setImageResource(R.drawable.leon);
+                        break;
+                    case DOG_IMG_ID:
+                    default:
+                        imageView.setImageResource(R.drawable.perro);
+                }
+                Log.w("setImg", "Wait 3 SECONDS");
+            });
 
-            Log.w("setImg", "Wait 3 SECONDS");
 
             // Wait for every second
             try {
-                Thread.sleep(IMAGE_VISUALIZATION_TIME); // 1 second
+                Thread.sleep(IMAGE_VISUALIZATION_TIME); // 3 second
             } catch (Exception ex) {
             }
 
-            Log.w("setImg", "Finish visualization");
+            runOnUiThread(()->{
+                Log.w("setImg", "Finish visualization");
 
-            Log.w("doInBackground", "imageIdToShow is " + this.imageId);
+                Log.w("doInBackground", "imageIdToShow is " + this.imageId);
 
-            // Hide image view
-            imageView.setVisibility(View.GONE);
+                // Hide image view
+                imageView.setVisibility(View.GONE);
 
-            if (this.imageId >= imageIds.length) {
-                // Main thread ends tasks
-                loadImageProgressBarr.setVisibility(View.GONE);
-                loadImageTextLabel.setVisibility(View.GONE);
-                endMessageTextView.setVisibility(View.VISIBLE);
-            } else {
-                // Display load image label and progress barr
-                loadImageProgressBarr.setVisibility(View.VISIBLE);
-                loadImageTextLabel.setVisibility(View.VISIBLE);
-            }
+                if (this.imageId == imageIds.length - 1) {
+                    // Main thread ends tasks
+                    loadImageProgressBarr.setVisibility(View.GONE);
+                    loadImageTextLabel.setVisibility(View.GONE);
+                    endMessageTextView.setVisibility(View.VISIBLE);
+                    endActivityButton.setVisibility(View.VISIBLE);
+                    hasActivityFinished = true;
+                } else {
+                    // Display load image label and progress barr
+                    loadImageProgressBarr.setVisibility(View.VISIBLE);
+                    loadImageTextLabel.setVisibility(View.VISIBLE);
+                }
+            });
         }
 
         private void publishProgress(int progressValue, int randomTime) {
             int progress = progressValue * 100 / randomTime;
 
-            loadImageProgressBarr.setProgress(progress * 10);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                loadImageProgressBarr.setProgress(progress, true);
+            } else {
+                loadImageProgressBarr.setProgress(progress);
+
+            }
         }
 
         @Override
@@ -142,11 +183,12 @@ public class LoadImagesActivity extends AppCompatActivity {
             int loadAleatoryTime = new Random().nextInt((MAX_ALEATORY_NUMBER - MIN_ALEATORY_NUMBER) + 1) + MIN_ALEATORY_NUMBER;
 
             // Make thread simulate is loading the image
-            for (i = 0; i <= loadAleatoryTime; i++) {
+            for (int i = 0; i <= loadAleatoryTime; i++) {
+                int finalI = i;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        publishProgress(i, loadAleatoryTime);
+                        publishProgress(finalI, loadAleatoryTime);
                     }
                 });
                 // Wait for every second
@@ -161,10 +203,12 @@ public class LoadImagesActivity extends AppCompatActivity {
                 @Override
                 public void run() {
                     publishProgress(loadAleatoryTime, loadAleatoryTime);
-                    setImg();
                 }
             });
+
+            setImg();
         }
+
     }
 
 }
